5f1cecf2a3fac2dd6d3cf531483c448098362085
Shradha Shrestha <44583012+ShradhaShrestha@users.noreply.github.com>
2020-02-09 14:09:28 -0500

    Update analyzeNetwork.py

--- analyzeNetwork.py
+++ analyzeNetwork.py
@@ -14,7 +14,7 @@ my_gis = GIS(profile="fire_brickHack")
 sample_cities = my_gis.content.search('title:"USA Major Cities" type:Feature Service owner:esri*',
                                       outside_org=True)[9]
 
-stops_cities = ['San Francisco', 'San Jose', 'Los Angeles', 'San Diego']
+stops_cities = ['San Francisco', 'San Jose']
 values = "'" + "', '".join(stops_cities) + "'"
 
 
@@ -23,7 +23,7 @@ stops_cities_fl = FeatureLayer(sample_cities.url + "/0")
 type(stops_cities_fl)
 
 
-stops_cities_fset = stops_cities_fl.query(where="ST in ('CA', 'NV', 'TX', 'AZ', 'LA', 'FL')  AND NAME IN ({0})".format(values), as_df=False)
+stops_cities_fset = stops_cities_fl.query(where="ST in ('CA')  AND NAME IN ({0})".format(values), as_df=False)
 
 def re_order_stop_cities(fset=stops_cities_fset, start_city="Miami", end_city="San Francisco"):
     stops_cities_flist = []
@@ -50,34 +50,3 @@ result = network.analysis.find_routes(re_ordered_stops_cities_fset, time_of_day=
                                       preserve_terminal_stops="Preserve None",
                                       reorder_stops_to_find_optimal_routes=True,
                                       save_output_na_layer=True)
-
-""" visualization
-"""
-styles = [
-    dict(selector="td", props=[("padding", "2px")]),
-    dict(selector='.row_heading, .blank', props=[('display', 'none;')]),
-    dict(selector='.col_heading, .blank', props=[('display', 'none;')])]
-
-route_symbol = {
-                    "type": "esriSLS",
-                    "style": "esriSLSSolid",
-                    "color": [128,0,128,90],
-                    "width": 4
-                }
-
-stops_symbol = {"angle":0,"xoffset":2,"yoffset":8,"type":"esriPMS",
-                "url":"http://static.arcgis.com/images/Symbols/Basic/ShinyPin.png",
-                "contentType":"image/png","width":24,"height":24}
-
-start_symbol = {"angle":0,"xoffset":0,"yoffset":8.15625,"type":"esriPMS",
-                "url":"http://static.arcgis.com/images/Symbols/AtoZ/redA.png",
-                "contentType":"image/png","width":15.75,"height":21.75}
-
-end_symbol = {"angle":0,"xoffset":0,"yoffset":8.15625,"type":"esriPMS",
-              "url":"http://static.arcgis.com/images/Symbols/AtoZ/greenB.png",
-              "contentType":"image/png","width":15.75,"height":21.75}
-
-popup_route = {"title": "Route",
-               "content": df.style.set_table_styles(styles).render()}
-popup_stop = {"title": "Stop {}",
-              "content": df.style.set_table_styles(styles).render()}
